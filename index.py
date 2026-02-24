858ce329f36c3ab8349a119404f937f0ce909493
ShyTech1 <shaikelman@gmail.com>
2026-02-24 18:00:58 +0200

    inital project

---
+++ index.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.get("/")
+def hello_world():
+    return render_template("index.html")
+
+
+@app.get("/api/chat/<room>")
+def get(room):
+    return f"We are at room {room}"
+
+@app.post("/api/chat/<room>") #post some data to the server
+def post():
+    "<h1>POST metod</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
